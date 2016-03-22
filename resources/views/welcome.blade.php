e8e6d24f34fe66052beba8f932daf6f7e67ec683
Tolik <anatoliymandrichenko@gmail.com>
2016-03-22 18:55:33 +0200

    init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Ivoice</title>
+    <link href="/css/app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<nav>
+    <div class="nav">
+        <h5>IVR-VOICE</h5>
+        <ul>
+            <li><a>ПОРТФОЛИО</a></li>
+            <li><a>ДИКТОРЫ</a></li>
+            <li><a>ОФОРМЛЕНИЕ</a></li>
+            <li><a>ПРАЙС</a></li>
+            <li><a>ОПЛАТА</a></li>
+        </ul>
+        <h6>КОНТАКТЫ</h6>
+    </div>
+</nav>
+<section class="intro">
+    <div class="title">
+        <h4>ГОЛОСОВЫЕ ПРИВЕТСТВИЯ</h4>
+        <h6>для телефонии и IVR</h6>
+        <button>OPEN</button>
+    </div>
+</section>
+<section class="services">
+    <h6>Мы предоставляем</h6>
+    <div class="service-text">
+        <div class="circle"></div>
+        <h6>РАЗРАБОТКА ТЕКСТОВ</h6>
+        <p>
+            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+            An quo
+            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+            natum at,
+            cetero meliore apeirian in vis.
+            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
+            Putent
+            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
+            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
+            et dicam impetus his. Semper
+            nominati explicari ex vim, diam autem moderatius qui et. Vel eu sint meliore vocibus. His possit
+            fabulas
+            praesent et, at eum iusto bonorum perpetua, nec viris vidisse voluptua ex.
+        </p>
+        <div class="bottom">
+            <a>ПОДРОБНЕЕ</a>
+        </div>
+    </div>
+    <div class="service-language">
+        <div class="circle"></div>
+        <h6>ИНОСТРАННЫЕ ДИКТОРЫ</h6>
+        <p>
+            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+            An quo
+            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+            natum at,
+            cetero meliore apeirian in vis.
+            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
+            Putent
+            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
+            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
+        </p>
+        <div class="bottom">
+            <a>ПОДРОБНЕЕ</a>
+        </div>
+    </div>
+    <div class="service-partner">
+        <div class="circle"></div>
+        <h6>ПАРТНЕРСКАЯ ПРОГРАММА</h6>
+        <p>
+            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+            An quo
+            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+            natum at,
+            cetero meliore apeirian in vis.
+            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
+            Putent
+            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
+            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
+        </p>
+        <div class="bottom">
+            <a>ПОДРОБНЕЕ</a>
+        </div>
+    </div>
+</section>
+</body>
+</html>
