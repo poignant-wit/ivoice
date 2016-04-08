72d35700d62f8d22f846dd0d6fa51ab221140f06
Tolik <anatoliymandrichenko@gmail.com>
2016-04-08 18:21:20 +0300

    css work main page

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,77 @@
-var elixir = require('laravel-elixir');
-require('laravel-elixir-sass-compass');
+//var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var postcss = require('gulp-postcss');
+var precss = require('precss');
+var sass = require('gulp-sass');
+var rename = require("gulp-rename");
+var lost = require("lost");
+
+
+
+
+gulp.task('styles', function(){
+    var processors = [
+        //precss({}),
+
+        require("postcss-autoreset")({
+            reset: {
+                margin: 0,
+                padding: 0,
+                borderRadius: 0
+            }
+        }),
+        lost,
+        require('postcss-font-magician')({ /* options */ })
+    ];
+   return gulp.src('resources/assets/postcss/app.scss')
+       .pipe(sass())
+       .pipe(postcss(processors))
+       .pipe(rename('app.css'))
+       .pipe(gulp.dest('public/css'))
+       .pipe(browserSync.stream());
+});
+
+
+
+
+
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('watch', function() {
+    browserSync.init({
+        proxy: 'http://homestead.app'
+    });
+
+    gulp.watch('resources/assets/postcss/*.scss', ['styles']);
+    gulp.watch('resources/assets/postcss/*/*/*.scss', ['styles']);
+    gulp.watch("resources/views/*.blade.php").on('change', browserSync.reload);
+});
+
+
+
+
+
+
+//elixir(function(mix) {
+//    mix.webpack(
+//        {
+//            module: {
+//                loaders: [
+//                    {
+//                        test: /\.css$/,
+//                        loader: "style-loader!css-loader!postcss-loader"
+//                    }
+//                ]
+//            },
+//            postcss: function () {
+//                return [require('autoprefixer'), require('precss')];
+//            }
+//
+//        }
+//
+//    );
+//});
 
 /*
  |--------------------------------------------------------------------------
@@ -12,10 +84,11 @@ require('laravel-elixir-sass-compass');
  |
  */
 
-elixir(function(mix) {
-    mix.compass("app.scss", "public/css", {
-            config_file: "config.rb"
-    })
-        .browserSync(
-        );
-});
+//elixir(function(mix) {
+//    mix.browserSync(
+//        );
+//});
+
+
+
+
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,6 +2,9 @@
 <html>
 <head>
     <title>Ivoice</title>
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700&subset=latin,cyrillic' rel='stylesheet'
+          type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Lobster&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
     <link href="/css/app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
@@ -25,64 +28,137 @@
         <button>OPEN</button>
     </div>
 </section>
-<section class="services">
-    <h6>Мы предоставляем</h6>
-    <div class="service-text">
-        <div class="circle"></div>
-        <h6>РАЗРАБОТКА ТЕКСТОВ</h6>
-        <p>
-            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
-            An quo
-            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
-            natum at,
-            cetero meliore apeirian in vis.
-            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
-            Putent
-            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
-            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
-            et dicam impetus his. Semper
-            nominati explicari ex vim, diam autem moderatius qui et. Vel eu sint meliore vocibus. His possit
-            fabulas
-            praesent et, at eum iusto bonorum perpetua, nec viris vidisse voluptua ex.
-        </p>
-        <div class="bottom">
-            <a>ПОДРОБНЕЕ</a>
+{{--services--}}
+<section class="services with-background-white-50">
+    <h6 class="section-header">Наши услуги
+    </h6>
+    <div class="section-header-line">
+    </div>
+    <div class="container">
+        <div class="service-block">
+            <div class="circle">
+                <div class="circle-inner">
+                    <img src="/img/three.png">
+                </div>
+            </div>
+            <h6>Разработка<br>текстов</h6>
+            <p>
+                Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+                An quo
+                dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+                natum at,
+                cetero meliore apeirian in vis.
+                An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
+                Putent
+            </p>
+            <div class="bottom">
+                <a>ПОДРОБНЕЕ</a>
+            </div>
+        </div>
+        <div class="service-block">
+            <div class="circle">
+                <div class="circle-inner">
+                    <img src="/img/multimedia.png">
+                </div>
+            </div>
+            <h6>Иностранные<br>дикторы</h6>
+            <p>
+                Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+                An quo
+                dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+                natum at,
+                cetero meliore apeirian in vis.
+            </p>
+            <div class="bottom">
+                <a>ПОДРОБНЕЕ</a>
+            </div>
+        </div>
+        <div class="service-block">
+            <div class="circle">
+                <div class="circle-inner">
+                    <img src="/img/gestures.png">
+                </div>
+            </div>
+            <h6>Партнерская<br>программа</h6>
+            <p>
+                Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
+                An quo
+                dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
+                natum at,
+                cetero meliore apeirian in vis.
+            </p>
+            <div class="bottom">
+                <a>ПОДРОБНЕЕ</a>
+            </div>
         </div>
     </div>
-    <div class="service-language">
-        <div class="circle"></div>
-        <h6>ИНОСТРАННЫЕ ДИКТОРЫ</h6>
-        <p>
-            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
-            An quo
-            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
-            natum at,
-            cetero meliore apeirian in vis.
-            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
-            Putent
-            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
-            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
-        </p>
-        <div class="bottom">
-            <a>ПОДРОБНЕЕ</a>
+</section>
+{{--end services--}}
+
+
+{{--clients--}}
+<section class="clients">
+    <h6 class="section-header">Наши клиенты</h6>
+    <div class="section-header-line">
+    </div>
+
+
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+
+</section>
+{{--end clients--}}
+
+
+
+<section class="comments with-background-white-300">
+    <h6 class="section-header">Отзывы</h6>
+    <div class="section-header-line">
+    </div>
+
+    <div class="container">
+
+        <div class="comment-block">
+            <div class="circle">
+                <img class="circle-inner" src="http://avatarbox.net/avatars/img26/the_terminator_avatar_picture_27066.jpg">
+            </div>
+            <h5>
+                Какой-то крутой чувак
+            </h5>
+            <h6>
+                Из такой-то конторы
+            </h6>
+            <div class="section-header-line">
+            </div>
+            <p>
+               «Это, мать его, лучшй сервис в мире. В жизни нчиего лучшего не встречал. Это круче, чем качалка»
+            </p>
+
+
+        </div>
+        <div class="comment-block">
+
+        </div>
+        <div class="comment-block">
+
         </div>
     </div>
-    <div class="service-partner">
-        <div class="circle"></div>
-        <h6>ПАРТНЕРСКАЯ ПРОГРАММА</h6>
-        <p>
-            Lorem ipsum dolor sit amet, ius movet postea audiam ex. Diam unum movet sed an, eam ea stet tollit.
-            An quo
-            dicant utinam, qui ne mundi viris. Doming vocent quo ut, in simul eripuit legimus mel. Eos sint
-            natum at,
-            cetero meliore apeirian in vis.
-            An has nonumy iriure convenire, nam sumo volutpat dissentiet ad, nemore ancillae deserunt pri at.
-            Putent
-            ceteros vivendum eu vim, quo ridens placerat elaboraret ne, eu aperiri detracto eos. Pro assueverit
-            conclusionemque an, ad vix quod debet errem. Sea atomorum prodesset id. Fastidii evertitur ei pro.
-        </p>
-        <div class="bottom">
-            <a>ПОДРОБНЕЕ</a>
+
+</section>
+<section class="how-order">
+    <h6 class="section-header">Как сделать заказ</h6>
+</section>
+<section class="with-background-blue">
+    <div class="container">
+        <div class="footer-block">
+            Форма отправки
+        </div>
+        <div class="footer-block">
+            Форма отправки
         </div>
     </div>
 </section>
